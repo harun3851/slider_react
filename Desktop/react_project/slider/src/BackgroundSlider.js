7eec122f946e61bc6e13c473217111ad7a8671c9
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-17 00:55:01 +0200

    set timer

--- Desktop/react_project/slider/src/BackgroundSlider.js
+++ Desktop/react_project/slider/src/BackgroundSlider.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './BackgroundSlider.css';
 import imageSlide from './data';
 
 
   const BackgroundSlider = () => {
-    const[currentState, setCurrentState]= useState(0)
+    const[currentState, setCurrentState]= useState(2);
+    useEffect(()=>{
+        const timer=setTimeout(()=>
+       {
+        if(currentState===2){
+            setCurrentState(0)
+        }else
+        {
+            setCurrentState(currentState+1)
+        }
+       },15000 )
+    },[currentState]);
     const bgImageStyle={
         backgroundImage:`url(${imageSlide[currentState].name})`,
         backgroundPosition:'center',
@@ -12,14 +23,24 @@ import imageSlide from './data';
         height:'100%',
 
     }
+    const goToNext=(currentState)=>{
+        setCurrentState(currentState);
+    }
   return (
     <div className='countiner-style'>
         <div style={bgImageStyle}> </div>
         <div className='description'>
             <div>
                 <h1>{imageSlide[currentState].title}</h1>       
-                <p>{imageSlide[currentState].body}</p>            </div>
-                <div className='carausel-boullt'></div>
+                <p>{imageSlide[currentState].body}</p>           
+            </div>
+                <div className='carausel-boullt'>
+                {
+                imageSlide.map((imageSlide,currentState)=>(
+                    <span key={currentState} onClick={() => goToNext(currentState)}></span>
+                ))
+            }
+                </div>
         </div>
     </div>
   );  

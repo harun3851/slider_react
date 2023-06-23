3fb0c996bc09dd593562db7deb602dd91284745d
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-06-23 15:55:37 +0300

    useState text changer

--- Desktop/react_project/react_hooks/react_hooks/src/App.js
+++ Desktop/react_project/react_hooks/react_hooks/src/App.js
@@ -1,22 +1,36 @@
-
-import './App.css';
-import React, {useState} from "react";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+  const [inputValue, setInputValue] = useState("harun");
 
-const[counter, setCounter]=useState(1);
-  
-  const increment= ()=> {
- setCounter(counter+1);
+  const increment = () => {
+    setCounter(counter + 1);
     console.log(counter);
   };
+ const onChange = (event) => {
+  const newValue = event.target.value;
+  setInputValue(newValue);
 
+ };
   return (
     <div className="App">
-      <p>{counter}</p>
-<button onClick={increment}> increment </button>
+      <div>
+        <p>example 1:statehooks increment</p>
+        <p>{counter}</p>
+        <button onClick={increment}> increment </button>
+        <p>_____________________</p>
+      </div>
+
+      <p>example 2:statehooks onchange</p>
+
+      <div style={{ marginTop: "40px" }}>
+        <input placeholder="write your name..." onChange={onChange}></input>
+        <p>{inputValue}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
